cd6ffffdb08b93a7afc3e502a0c8568e5a7512ef
Water Blower <127284497+waterblower@users.noreply.github.com>
2026-02-21 00:23:38 +0800

    Update run.zig

--- run.zig
+++ run.zig
@@ -209,17 +209,16 @@ fn read_checkpoint(a: std.mem.Allocator, checkpoint_path: []const u8) !*Transfor
 
     try fd.seekTo(0);
 
-    // This was missing. We ask for 'file_size' bytes aligned to 4096.
-    t.data = try a.alignedAlloc(
-        u8,
-        std.mem.Alignment.fromByteUnits(4096),
-        t.file_size,
-    );
-    // Safety: If readAll fails, free this memory
-    errdefer a.free(t.data);
-
     // 6. Memory Map
     if (comptime builtin.os.tag == .windows) {
+        // This was missing. We ask for 'file_size' bytes aligned to 4096.
+        t.data = try a.alignedAlloc(
+            u8,
+            std.mem.Alignment.fromByteUnits(4096),
+            t.file_size,
+        );
+        // Safety: If readAll fails, free this memory
+        errdefer a.free(t.data);
         const bytes_read = try fd.readAll(t.data);
         if (bytes_read != t.file_size) {
             return error.IncompleteRead;
@@ -334,9 +333,14 @@ const Transformer = struct {
         // 2. Free the main model data buffer
         // Since we switched to readAll + alignedAlloc, we use allocator.free.
         // If you revert to mmap later, change this to std.posix.munmap(self.data);
-        if (self.data.len > 0) {
-            self.a.free(self.data);
+        if (comptime builtin.os.tag == .windows) {
+            if (self.data.len > 0) {
+                self.a.free(self.data);
+            }
+        } else {
+            std.posix.munmap(self.data);
         }
+
         self.a.destroy(self);
     }
 };
